40177b9255c71f5adb91f0764eb7d710a7bbe0d4
klar2d2 <koberyan@comcast.net>
2019-09-15 09:53:18 -0700

    checking for a merger

--- client/src/components/ShowArt.tsx
+++ client/src/components/ShowArt.tsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 class ShowArt extends Component {
   render() {
     return(
-
+      <div>
+        Show the art individually: TODO pop up with a close out window.
+      </div>
     )
   }
 }
