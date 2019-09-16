0bb4e1f79c200bac978c92668e954849d3b42530
KHilse <khilsedev@gmail.com>
2019-09-16 16:17:04 -0700

    merge

--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -20,15 +20,12 @@ export interface AppProps {
   getUser?(): string
 }
 
-<<<<<<< HEAD
 interface IPost {
   id: string;
   media_type: string;
   media_url: string;
   timestamp: string;
  }
-=======
->>>>>>> f0cc07b5ab67d7da34c541ace257d94cec2a3118
 export interface ITest {
   albumId: number,
 
