6be31aab9caddf1558c9d1d1d5699dc3fc67f6a4
KHilse <khilsedev@gmail.com>
2019-09-17 09:26:35 -0700

    Added instagram token to vendor model, removed build folders from repo

--- src/models/vendor.ts
+++ src/models/vendor.ts
@@ -6,6 +6,12 @@ export const VendorSchema: Schema = new Schema({
   address: AddressSchema,
   phoneNumber: String,
   website: String,
+  instagramAccessToken: String
 });
+
+VendorSchema.methods.encryptToken = function (token: String) : String {
+  // TODO Add encryption method
+  return '';
+}
 
 export const Vendor: Model<IVendorModel> = model<IVendorModel>("Vendor", VendorSchema);
